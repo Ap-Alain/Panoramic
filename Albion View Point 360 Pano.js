9a380fcd928a1d02db1b312b5215dba9901c0952
Alain Ramgurrib <148376532+Ap-Alain@users.noreply.github.com>
2024-11-25 22:38:41 +0400

    Add files via upload

---
+++ Albion View Point 360 Pano.js
@@ -0,0 +1,43 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#000; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 400; text-align:left; border-radius: 3px; border: rgba(0,0,0,0.66) solid 1px; box-shadow: 2px 2px 3px 0px rgba(0,0,0,0.66);background: rgba(255,255,255,0.75);"
+    },
+"include": [
+        {
+            "src": "Albion View Point 360 Pano_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 0.08,
+	        "reference": "screen",
+	        "restart": 5,
+	        "returnToLevel": true,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 360,
+	        "maxtilt": 9.80787926484885,
+	        "minpan": 0,
+	        "mintilt": -9.80787926484885
+	    },
+	"id": "Albion View Point 360 Pano",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_1b_%0y_%0x.jpg"},"down":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_1d_%0y_%0x.jpg"},"front":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_1f_%0y_%0x.jpg"},"height":27503,"left":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_1l_%0y_%0x.jpg"},"right":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_1r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_1u_%0y_%0x.jpg"},"width":27503},{"back":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_2b_%0y_%0x.jpg"},"down":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_2d_%0y_%0x.jpg"},"front":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_2f_%0y_%0x.jpg"},"height":13752,"left":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_2l_%0y_%0x.jpg"},"right":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_2r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_2u_%0y_%0x.jpg"},"width":13752},{"back":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_3b_%0y_%0x.jpg"},"down":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_3d_%0y_%0x.jpg"},"front":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_3f_%0y_%0x.jpg"},"height":6876,"left":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_3l_%0y_%0x.jpg"},"right":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_3r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_3u_%0y_%0x.jpg"},"width":6876},{"back":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_4b_%0y_%0x.jpg"},"down":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_4d_%0y_%0x.jpg"},"front":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_4f_%0y_%0x.jpg"},"height":3438,"left":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_4l_%0y_%0x.jpg"},"right":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_4r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_4u_%0y_%0x.jpg"},"width":3438},{"back":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_5b_%0y_%0x.jpg"},"down":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_5d_%0y_%0x.jpg"},"front":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_5f_%0y_%0x.jpg"},"height":1719,"left":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_5l_%0y_%0x.jpg"},"right":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_5r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_5u_%0y_%0x.jpg"},"width":1719},{"back":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_6b_%0y_%0x.jpg"},"down":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_6d_%0y_%0x.jpg"},"front":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_6f_%0y_%0x.jpg"},"height":860,"left":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_6l_%0y_%0x.jpg"},"right":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_6r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_6u_%0y_%0x.jpg"},"width":860},{"back":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_7b_%0y_%0x.jpg"},"down":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_7d_%0y_%0x.jpg"},"front":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_7f_%0y_%0x.jpg"},"height":430,"left":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_7l_%0y_%0x.jpg"},"right":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_7r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_t_7u_%0y_%0x.jpg"},"width":430}],"multilevel":true,"preview":{"src":"Albion View Point 360 Pano_tiles/Albion View Point 360 Pano_preview.jpg"},"projection":"cubic"},
+	"view": {
+	        "hfov": 29.42,
+	        "pan": 180,
+	        "tilt": 0
+	    }
+},
+"version": "4.2"
+}
+}
